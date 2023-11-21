36351a18ede9f94d94b9471cc43b35de2e04d40c
Katherine Paucar <gaby2091@outlook.es>
2023-11-21 20:52:15 +0100

    Update 3-clone-merge.ts
    Controlar que sea un objeto

--- src/02-entregables/3-clone-merge.ts
+++ src/02-entregables/3-clone-merge.ts
@@ -1,10 +1,18 @@
+console.log('------------------------INICIO EJERCICIO 3-----------------------');
 //CLONE
+
 const dataPerson = {"name": "Daniela", "lastName": "Andrade", "age":25}
-function clone(source) {
-   const copy = {...source}
-   return copy;
+function clone (source) {
+    if(typeof source === 'object'){
+        const copy = {...source}
+        return copy;
+    }
+    return 'No es un objeto';
+
 }
 console.log('CLONE:', clone(dataPerson));
+console.log('CLONE:', clone(2));
+
 console.log('---------------------------');
 
 // MERGE
@@ -14,8 +22,12 @@ const b = { name: "Luisa", age: 31, married: true };
  * merge(a, b); // {name: "Maria", age: 31, married: true, surname: "Ibañez", country: "SPA"}
  */
 function merge(source, target) {
-    const mergeData = {...target, ...source};
-    return mergeData;
+    if(typeof source === 'object' && typeof target === 'object'){
+        const mergeData = {...target, ...source};
+        return mergeData;
+    }
+    return 'Alguno de los parámetros introducidos no es un objeto'
 }
 console.log('MERGE:', merge(a,b));
-console.log('---------------------------');
+console.log('MERGE:', merge(2,b));
+console.log('------------------------FIN EJERCICIO 3-----------------------');
