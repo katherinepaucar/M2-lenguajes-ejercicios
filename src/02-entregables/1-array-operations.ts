fba2079da009b39f577bb9732ab9d94e291d5756
Katherine Paucar <gaby2091@outlook.es>
2023-11-21 21:15:55 +0100

    Update 1-array-operations.ts

--- src/02-entregables/1-array-operations.ts
+++ src/02-entregables/1-array-operations.ts
@@ -1,6 +1,6 @@
 console.log('------------------------INICIO EJERCICIO 1-----------------------');
 // --------HEAD---------
-const head = <T>(array: Array<T>) => {
+const head = <T>(array: Array<T>):T => {
     const [first] = array;
     return first
 };
@@ -12,7 +12,7 @@ console.log('HEAD ages:',head(ages));
 console.log('---------------------------------');
 
 //---------Tail-------------
-const tail = <T>(array: Array<T>) => {
+const tail = <T>(array: Array<T>): Array<T>=>  {
     const [,...data] = array;
     return data;
 
@@ -21,7 +21,7 @@ console.log('TAIL names:',tail(names));
 console.log('TAIL ages:',tail(ages));
 console.log('---------------------------------');
 //------ INIT --------------
-const init = <T>(data: Array<T>) => {
+const init = <T>(data: Array<T>): Array<T>  => {
     const  result = data.slice(0, data.length - 1);
     return result;
 };
@@ -29,10 +29,11 @@ console.log('INIT names:',init(names));
 console.log('INIT ages:', init(ages));
 console.log('---------------------------------');
 //------ LAST --------------
-const last = <T>(data: Array<T>) => {
+const last = <T>(data: Array<T>): Array<T>=> {
     const  last =  data.slice(data.length - 1);
     return last;
 };
 console.log('LAST names:',last(names));
 console.log('LAST ages:', last(ages));
+
 console.log('------------------------FIN EJERCICIO 1-----------------------');
