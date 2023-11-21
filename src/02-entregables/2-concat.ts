ed964b60af62f006bf7dbaa16dc8392875cd9929
Katherine Paucar <gaby2091@outlook.es>
2023-11-21 20:39:54 +0100

    Update 2-concat.ts
    tipar datos

--- src/02-entregables/2-concat.ts
+++ src/02-entregables/2-concat.ts
@@ -1,7 +1,8 @@
+console.log('------------------------INICIO EJERCICIO 2-----------------------');
 const fruits = ['apple','orange','banana', 'melon'];
 const animals = ['dog','cat', 'bird','horse'];
 const numbers = [2,3,4,8,6];
-const concat = (a, b) => {
+const concat = <T>(a: Array<T>, b: Array<T>) => {
     const result = [...a,...b]
     return result;
 }; 
@@ -9,9 +10,9 @@ const concat = (a, b) => {
 console.log('Concat: ', concat(fruits, animals));
 console.log('----------------------------------------');
 /// Opcional
-const concatV2 = (...array) => {
-    const concatArray = [];
-    array.map(item => {
+const concatV2 = (...array ) => {
+    const concatArray= [];
+    array.map((item) => {
         // console.log(item);
         concatArray.push(...item)
     });
@@ -20,3 +21,4 @@ const concatV2 = (...array) => {
 }; 
 
 console.log('Opcional - concat: ', concatV2(fruits, animals, numbers));
+console.log('------------------------INICIO EJERCICIO 2-----------------------');
